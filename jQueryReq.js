8d0bcaf248c60dbe82d9200c62c3b17d3b182f5e
etliu <eric.t.liu15@gmail.com>
2017-01-18 20:56:07 -0800

    added some dependencies and updated the javascript

--- jQueryReq.js
+++ jQueryReq.js
@@ -5,15 +5,29 @@ $(document).ready(function () {
 
   console.log(spreadsheetURL)
 
+  function generateTimestamp(time) {
+    var strarray = time.split(/[/:\s]/);
+    console.log(strarray);
+    var mon = {"01":"January", "02":"Feburary", "03":"March", "04":"April",
+      "05":"May", "06":"June", "07":"July", "08":"August",
+      "09":"September", "10":"October", "11":"November", "12":"December"};
+    strarray[1] = mon[strarray[1]];
+    strarray[0] = parseInt(strarray[0]);
+    strarray[5] = (parseInt(strarray[3]) / 12 > 1)?"PM": "AM";
+    strarray[3] = parseInt(strarray[3])%12;
+    return strarray[1] + " " + strarray[0] + " at " + strarray[3] + ":" + strarray[4] + " " + strarray[5];
+  }
+
   $.getJSON(spreadsheetURL, function(data) {
     for(entry of data.feed.entry){
       var context = {};
       context.title = entry.gsx$title.$t;
       context.content = "<p>" + entry.gsx$content.$t + "</p>";
+      context.timestamp = generateTimestamp(entry.gsx$timestamp.$t)
       console.log(context)
       var temp = template(context);
       console.log(temp);
-      $("#bloggrid").append(temp);
+      $("#insertionpoint").prepend(temp);
     }
   })
 
